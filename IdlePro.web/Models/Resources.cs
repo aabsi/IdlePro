d227d1cc70cd98439623193e7a2da0e00c4d1819
aabsi <aabsi@uwo.ca>
2022-08-04 02:04:00 -0400

    Add project files.

---
+++ IdlePro.web/Models/Resources.cs
@@ -0,0 +1,20 @@
+﻿namespace IdlePro.web.Models
+{
+    public class Resources
+    {
+        public int gold { get; set; }
+        public int wood { get; set; }
+        public int stone { get; set; }
+        public int food { get; set; }
+        public int people { get; set; }
+
+        public Resources(int gold, int wood, int stone, int food, int people)
+        {
+            this.gold = gold;
+            this.wood = wood;
+            this.stone = stone;
+            this.food = food;
+            this.people = people;
+        }
+    }
+}
---
+++ IdlePro.web/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿
+var wood = 0;
+var gold = 0;
+var pop = 0;
+var souls = 0;
+
+document.getElementById("wood").innerHTML = wood;
+document.getElementById("gold").innerHTML = gold;
+document.getElementById("pop").innerHTML = pop;
+document.getElementById("souls").innerHTML = souls;
+
+function chopWood() {
+    wood = wood + 2;
+    document.getElementById("wood").innerHTML = wood;
+}
